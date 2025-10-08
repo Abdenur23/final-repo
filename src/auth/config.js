baf36c1da549252a68fcd30b31e5a2614d88fc29
Abdenur23 <123627297+Abdenur23@users.noreply.github.com>
2025-10-08 00:07:25 -0700

    Create config.js

---
+++ src/auth/config.js
@@ -0,0 +1,7 @@
+const cognitoConfig = {
+  UserPoolId: 'us-east-1_XXXXXXXXX', // You'll need to add your User Pool ID here
+  ClientId: '7irso7dmmnp793egs9bhkl0t81',
+  Domain: 'auth.theweer.com'
+};
+
+export default cognitoConfig;
